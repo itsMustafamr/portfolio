95a5feeda7474626abe143cfdba37c083cf207ab
mohd7 <mohd7@iastate.edu>
2024-12-04 03:20:18 -0600

    Linked frontend with backend - flask message on homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,31 @@
-import React from 'react';
+// import React from 'react';
+
+// function Home() {
+//     return (
+//         <div>
+//             <h2>Home</h2>
+//             <p>Welcome to my portfolio website!</p>
+//         </div>
+//     );
+// }
+
+// export default Home;
+
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5001/api/hello')
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+    }, []);
+
     return (
         <div>
             <h2>Home</h2>
-            <p>Welcome to my portfolio website!</p>
+            <p>{message}</p>
         </div>
     );
 }
